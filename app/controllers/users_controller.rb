51b835b3cf654eb64af3c63c79128ca05a28381a
fanzhong <fanzhongzeng78@gmail.com>
2020-02-04 12:01:54 -0500

    created basic backend, can read from route /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+		render json: @users, only: [:id, :name, :email, :profile_pic],include: {interests: {only: [:id, :user, :interest]}}
+	end
+
+	def create
+		whitelist = "LOL this is gonna be annoying to do"
+
+		@User = User.create(whitelist)
+		if @User.valid?
+			render json: @User
+		else
+			render json: {errors: @User.errors.full_messages}
+		end
+	end
+
+	def show
+		puts "Need to check token somehow, auth stuff"
+	end
+
+	def login
+		puts "Time to do Auth...lol"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :interests
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login', to: 'users#login'
 end
