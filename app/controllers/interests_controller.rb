ed982f50af26d3665959b6e03c4a8cec7f1aa7f5
fanzhong <fanzhongzeng78@gmail.com>
2020-02-12 17:57:48 -0500

    finished deleting interest

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,13 +1,22 @@
 class InterestsController < ApplicationController
 	def create 
-		whitelist = params.permit(:user, :interest)
+		@user = User.find(params[:user_id])
 
-		@interest = Interest.create(whitelist)
-		if @interest.valid?
-			render json: {interest: @interest}
+		#TODO: Want to check user with token
+		#If user is valid, then we create the interest
+		if @user.valid?
+			interest = params[:interest]
+
+			@interest = Interest.create({user: @user, interest: interest})
+			if @interest.valid?
+				render json: {interest: @interest}
+			else
+				render json: {errors: @interest.errors.full_messages}
+			end
 		else
-			render json: {errors: @interest.errors.full_messages}
+			render json: {errors: @user.errors.full_messages}
 		end
+
 	end
 
 	def destroy
